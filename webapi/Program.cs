370bf33624ca3cce8ff5a785bc1e902c19f771d3
Raphael Santos <raphaelmacsa@gmail.com>
2019-05-26 15:23:57 -0200

    Adding webapi project using ASP.NET Core 2.0

---
+++ webapi/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace UpcomingMovies.Api
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args)
+        {
+            var port = Environment.GetEnvironmentVariable("PORT");
+            return WebHost.CreateDefaultBuilder(args)
+            .UseUrls($"http://+:{port}")
+            .UseStartup<Startup>()
+            .Build();
+        }
+            
+    }
+}
---
+++ webapi/README.md
@@ -0,0 +1,2 @@
+# upcoming-movies-mvp
+Repository for Upcoming Movies Web Application
