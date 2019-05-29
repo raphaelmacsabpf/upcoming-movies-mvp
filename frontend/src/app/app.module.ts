f29e8d16a442796319b6a6eeea64cef9d7fc71fd
Raphael Santos <raphaelmacsa@gmail.com>
2019-05-29 05:38:08 -0200

    [frontend] Add search bar

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
-import { MatCardModule, MatButtonModule} from '@angular/material';
+import { MatCardModule, MatButtonModule, MatIconModule} from '@angular/material';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -19,7 +19,8 @@ import { UpcomingMoviesService } from './services/upcoming-movies.service';
     HttpModule,
     AppRoutingModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [UpcomingMoviesService],
   bootstrap: [AppComponent]
